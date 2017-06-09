c5a9f20e355e07b1737aa3a759ade0940a7a308c
Getnway <Getnway@gmail.com>
2017-06-09 15:38:51 +0800

    * [DoubleTextView]add java method; add text right hint and bg

--- app/src/main/java/com/getnway/doubletextview/DoubleTextView.java
+++ app/src/main/java/com/getnway/doubletextview/DoubleTextView.java
@@ -3,8 +3,11 @@ package com.getnway.doubletextview;
 import android.content.Context;
 import android.content.res.ColorStateList;
 import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.ScaleDrawable;
+import android.os.Build;
+import android.support.annotation.DrawableRes;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.util.TypedValue;
@@ -20,7 +23,7 @@ import android.widget.TextView;
  * Author: getnway
  * Date: 17-06-09
  */
-public class DoubleTextView extends LinearLayout {
+public class DoubleTextView extends LinearLayout implements View.OnLayoutChangeListener {
     private static final int MAX_LEVEL = 10000; // ScaleDrawable#MAX_LEVEL
     private static final int DEFAULT_TEXT_SIZE = 15;
     private TextView textLeft, textRight;
@@ -44,6 +47,10 @@ public class DoubleTextView extends LinearLayout {
                 a.getDimensionPixelSize(R.styleable.DoubleTextView_textLeftPaddingRight, 0), 0);
         textLeft.setGravity(a.getInt(R.styleable.DoubleTextView_textLeftGravity, Gravity.CENTER_VERTICAL));
         textLeft.setLayoutParams(new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        leftDrawable = a.getDrawable(R.styleable.DoubleTextView_drawableLeft);
+        leftDrawablePadding = a.getDimensionPixelSize(R.styleable.DoubleTextView_drawableLeftPadding, 0);
+        leftDrawableScale = a.getFloat(R.styleable.DoubleTextView_drawableLeftScale, 1.0f);
+        setTextViewDrawable(textLeft, leftDrawable, leftDrawablePadding, leftDrawableScale);
 
         textRight.setText(a.getText(R.styleable.DoubleTextView_textRight));
         ColorStateList textRightColor = a.getColorStateList(R.styleable.DoubleTextView_textRightColor);
@@ -55,49 +62,84 @@ public class DoubleTextView extends LinearLayout {
                 a.getDimensionPixelSize(R.styleable.DoubleTextView_textRightPaddingRight, 0), 0);
         textRight.setGravity(textRightGravity | Gravity.CENTER_VERTICAL);
         textRight.setLayoutParams(new LayoutParams(0, ViewGroup.LayoutParams.MATCH_PARENT, 1.0f));
-
-        leftDrawable = a.getDrawable(R.styleable.DoubleTextView_drawableLeft);
-        if (leftDrawable != null) {
-            textLeft.setCompoundDrawablePadding(a.getDimensionPixelSize(R.styleable.DoubleTextView_drawableLeftPadding, 0));
-            float leftDrawableScale = a.getFloat(R.styleable.DoubleTextView_drawableLeftScale, 1.0f);
-            if (leftDrawableScale != 1.0f && leftDrawableScale > 0) {
-                ScaleDrawable leftScaleDrawable = new ScaleDrawable(leftDrawable, Gravity.CENTER, 1.0f, 1.0f);
-                leftScaleDrawable.setLevel((int) (leftDrawableScale * MAX_LEVEL));
-                leftDrawable = leftScaleDrawable;
-            }
-            Log.d(getClass().getSimpleName(), String.format("DoubleTextView: leftDrawable:%s leftDrawableScale:%s", leftDrawable, leftDrawableScale));
-            textLeft.setCompoundDrawablesWithIntrinsicBounds(leftDrawable, null, null, null);
-            // If the text is changed, we need to re-register the Drawable to recompute the bounds given the new TextView height
-            textLeft.addOnLayoutChangeListener(new OnLayoutChangeListener() {
-                @Override
-                public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
-                    textLeft.setCompoundDrawablesWithIntrinsicBounds(leftDrawable, null, null, null);
-                }
-            });
+        textRight.setHint(a.getString(R.styleable.DoubleTextView_textRightHint));
+        textRight.setHintTextColor(a.getColor(R.styleable.DoubleTextView_textRightHintColor, Color.parseColor("#666666")));
+        Drawable rightBg = a.getDrawable(R.styleable.DoubleTextView_textRightBackground);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            textRight.setBackground(rightBg);
+        } else {
+            textRight.setBackgroundDrawable(rightBg);
         }
-
         rightDrawable = a.getDrawable(R.styleable.DoubleTextView_drawableRight);
-        if (rightDrawable != null) {
-            textRight.setCompoundDrawablePadding(a.getDimensionPixelSize(R.styleable.DoubleTextView_drawableRightPadding, 0));
-            float rightDrawableScale = a.getFloat(R.styleable.DoubleTextView_drawableRightScale, 1.0f);
-            if (rightDrawableScale != 1.0f && rightDrawableScale > 0) {
-                ScaleDrawable rightScaleDrawable = new ScaleDrawable(rightDrawable, Gravity.CENTER, 1.0f, 1.0f);
-                rightScaleDrawable.setLevel((int) (rightDrawableScale*MAX_LEVEL));
-                rightDrawable = rightScaleDrawable;
-            }
-            // If the text is changed, we need to re-register the Drawable to recompute the bounds given the new TextView height
-            textRight.setCompoundDrawablesWithIntrinsicBounds(null, null, rightDrawable, null);
-            textRight.addOnLayoutChangeListener(new OnLayoutChangeListener() {
-                @Override
-                public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
-                    textRight.setCompoundDrawablesWithIntrinsicBounds(null, null, rightDrawable, null);
-                }
-            });
-        }
+        rightDrawablePadding = a.getDimensionPixelSize(R.styleable.DoubleTextView_drawableRightPadding, 0);
+        rightDrawableScale = a.getFloat(R.styleable.DoubleTextView_drawableRightScale, 1.0f);
+        setTextViewDrawable(textRight, rightDrawable, rightDrawablePadding, rightDrawableScale);
+
         a.recycle();
         setOrientation(HORIZONTAL);
         setClickable(true);
         addView(textLeft);
         addView(textRight);
+    }
+
+    public TextView getTextLeft() {
+        return textLeft;
+    }
+
+    public TextView getTextRight() {
+        return textRight;
+    }
+
+    private int leftDrawablePadding, rightDrawablePadding;
+    private float leftDrawableScale, rightDrawableScale;
+
+    public void setLeftDrawableScale(float drawableScale) {
+        this.leftDrawableScale = drawableScale;
+        setTextViewDrawable(textLeft, leftDrawable, leftDrawablePadding, leftDrawableScale);
+    }
+
+    public void setRightDrawableScale(float drawableScale) {
+        this.rightDrawableScale = drawableScale;
+        setTextViewDrawable(textLeft, leftDrawable, leftDrawablePadding, leftDrawableScale);
+    }
+
+    public void setTextLeftDrawable(Drawable drawable) {
+        this.leftDrawable = drawable;
+        setTextViewDrawable(textLeft, leftDrawable, leftDrawablePadding, leftDrawableScale);
+    }
+
+    public void setTextRightDrawable(Drawable drawable) {
+        this.rightDrawable = drawable;
+        setTextViewDrawable(textRight, rightDrawable, rightDrawablePadding, rightDrawableScale);
+    }
+
+    private void setTextViewDrawable(TextView textView, Drawable drawable, int drawablePadding, final float drawableScale) {
+        textView.setCompoundDrawablePadding(drawablePadding);
+        if (drawableScale != 1.0f && drawableScale > 0) {
+            ScaleDrawable rightScaleDrawable = new ScaleDrawable(drawable, Gravity.CENTER, 1.0f, 1.0f) {
+                @Override
+                public int getIntrinsicHeight() {
+                    return (int) (super.getIntrinsicHeight() * drawableScale);
+                }
+
+                @Override
+                public int getIntrinsicWidth() {
+                    return (int) (super.getIntrinsicWidth() * drawableScale);
+                }
+            };
+            rightScaleDrawable.setLevel((int) (drawableScale * MAX_LEVEL));
+            drawable = rightScaleDrawable;
+        }
+        // If the text is changed, we need to re-register the Drawable to recompute the bounds given the new TextView height
+        textView.addOnLayoutChangeListener(this);
+        textView.setCompoundDrawablesWithIntrinsicBounds(null, null, drawable, null);
+    }
+
+    @Override
+    public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+        if (leftDrawable != null)
+            textLeft.setCompoundDrawablesWithIntrinsicBounds(leftDrawable, null, null, null);
+        if (rightDrawable != null)
+            textRight.setCompoundDrawablesWithIntrinsicBounds(null, null, rightDrawable, null);
     }
 }
--- app/src/main/java/com/getnway/doubletextview/MainActivity.java
+++ app/src/main/java/com/getnway/doubletextview/MainActivity.java
@@ -2,12 +2,28 @@ package com.getnway.doubletextview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private DoubleTextView dtv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dtv = (DoubleTextView) findViewById(R.id.dtv);
+        dtv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String rightText = dtv.getTextRight().getText().toString().trim();
+                if ("".equals(rightText)) {
+                    dtv.getTextRight().setText("再点击一次");
+                } else {
+                    dtv.getTextRight().setText("");
+                }
+            }
+        });
     }
 }
